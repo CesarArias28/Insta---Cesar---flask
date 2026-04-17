55aaa2e640f8fd7d090f2a98218b21f257458191
CesarArias28 <cesararias2420@gmail.com>
2026-04-17 11:19:25 +0000

    final_commit

--- src/models.py
+++ src/models.py
@@ -1,19 +1,108 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Boolean, ForeignKey
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    fullname: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    posts: Mapped[list["Post"]] = relationship(back_populates="author")
+    comments: Mapped[list["Comment"]] = relationship(back_populates="author")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "fullname": self.fullname,
+            "phone": self.phone,
+
+        }
+
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    post: Mapped["Post"] = relationship(back_populates="media")
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image_url": self.image_url,
+            "user_id": self.user_id,
+            "type": self.type,
+            "post_id": self.post_id
+        }
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    caption: Mapped[str] = mapped_column(String(500))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+
+    author: Mapped["User"] = relationship(back_populates="posts")
+    comments: Mapped[list["Comment"]] = relationship(back_populates="post")
+    media: Mapped[list["Media"]] = relationship(back_populates="post")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image_url": self.image_url,
+            "caption": self.caption,
+            "user_id": self.user_id,
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    comment: Mapped[str] = mapped_column(String(500), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"), nullable=False)
+
+    author: Mapped["User"] = relationship(back_populates="comments")
+    post: Mapped["Post"] = relationship(back_populates="comments")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+
+
+class Follower(db.Model):
+    __tablename__ = 'follower'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id,
         }
